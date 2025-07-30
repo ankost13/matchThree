8e93aad5956a225cb4b90262eb61efbaeae55847
Melnyk Anastasia <a.melnyk@thunderspin.com>
2025-07-30 18:26:24 +0300

    Prohibition to scale

--- src/utils/resizeManager.js
+++ src/utils/resizeManager.js
@@ -7,6 +7,7 @@ export class ResizeManager {
         this.onResize();
         const resizeEvent = new Event("resize");
         window.dispatchEvent(resizeEvent);
+        this.preventPageZoom()
     }
 
     registerUI(view) {
@@ -34,6 +35,27 @@ export class ResizeManager {
                 });
             }, 0)
         });
+
+    }
+
+    preventPageZoom() {
+        window.addEventListener('wheel', (e)=> {
+            if (e.ctrlKey) {
+                e.preventDefault()
+            }
+        }, { passive: false })
+
+        window.addEventListener('keydown', (e)=> {
+            if (e.ctrlKey || e.metaKey) {
+                const zoomKeys = ['+', '-', '=', '0']
+                if (zoomKeys.includes(e.key)) {
+                    e.preventDefault()
+                }
+                if ([187, 189, 48].includes(e.keyCode)) {
+                    e.preventDefault()
+                }
+            }
+        })
 
     }
 
